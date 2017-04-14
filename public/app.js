778d926365d4b3697a57ca18b432ec741638e5da
Wei Yang <wei@weizilla.com>
2017-04-13 22:12:06 -0500

    Add Vue Resource, hit actual Divvy feed URL

--- public/app.js
+++ public/app.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-	el: "#app",
-	data: {
-		message: "Hello vue!"
-	}
+    el: "#app",
+    data: {
+        message: "Hello vue!"
+    },
+    created: function() {
+        this.$http.get("https://crossorigin.me/https://feeds.divvybikes.com/stations/stations.json").then(response => {
+            console.log(response.data)
+        }, response => {
+            console.log("Error: " + response)
+        });
+    }
 })
