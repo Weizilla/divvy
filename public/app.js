a609df9d891d3bd3128135f7eac8523542ff3922
Wei Yang <wei@weizilla.com>
2017-04-13 22:42:16 -0500

    Finish components, make look nice

--- public/app.js
+++ public/app.js
@@ -1,12 +1,22 @@
 var app = new Vue({
     el: "#app",
     data: {
-        message: "Hello vue!"
+        stations: [],
+        favIds: [300, 226, 117]
+    },
+    methods: {
+        available: function(num) {
+            return "h1 " + (num > 0 ? "text-success" : "text-danger");
+        }
     },
     created: function() {
         this.$http.get("https://crossorigin.me/https://feeds.divvybikes.com/stations/stations.json").then(response => {
-            console.log(response.data)
+            this.stations = response.data.stationBeanList
+                .filter((station) => this.favIds.indexOf(station.id) > -1)
+                .sort((a, b) => a.stationName.localeCompare(b.stationName))
+            console.log(JSON.stringify(this.stations))
         }, response => {
+            this.stations = []
             console.log("Error: " + response)
         });
     }
