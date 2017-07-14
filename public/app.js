d7b870bb6fb2eea7445a750ee17ea109981c8b67
Wei Yang <wei@weizilla.com>
2017-07-13 21:34:30 -0500

    More progress

--- public/app.js
+++ public/app.js
@@ -1,9 +1,16 @@
 var app = new Vue({
     el: "#app",
     data: {
-        stations: [],
+        stations: {},
         favs: {300: "Gym", 226: "Home", 117: "CTA L"},
-        favRoutes: [(226, 117), (226, 300), (300, 117), (300, 226), (117, 300), (117, 226)],
+        favRoutes: [
+            {start: 226, end: 117}, 
+            {start: 226, end: 300},
+            {start: 300, end: 117},
+            {start: 300, end: 226},
+            {start: 117, end: 300},
+            {start: 117, end: 226}
+        ],
         routes: []
     },
     methods: {
@@ -16,16 +23,43 @@ var app = new Vue({
         addName: function(station) {
             station.name = this.favs[station.id.toString()];
             return station;
+        },
+        genRoutes: function() {
+            this.routes = this.favRoutes.map(r => this.genRoute(r))
+            // console.log(this.routes);
+        },
+        genRoute: function(route) {
+            let start = this.stations[route.start];
+            let end = this.stations[route.end];
+            return {
+                start: start,
+                end: end,
+                status: this.calcStatus(start, end)
+            };
+        },
+        calcStatus: function(start, end) {
+            if (start.availableBikes == 0 || end.availableDocks == 0) {
+                return "panel-danger";
+            } else if (start.availableBikes > 2 && end.availableDocks > 2) {
+                return "panel-success";
+            } else {
+                return "panel-warning";
+            }
         }
     },
     created: function() {
         this.$http.get("https://crossorigin.me/https://feeds.divvybikes.com/stations/stations.json").then(response => {
-            this.stations = response.data.stationBeanList
-                .filter(station => this.isFavorite(station))
-                .map(station => this.addName(station))
-                .sort((a, b) => a.stationName.localeCompare(b.stationName));
+            this.stations = {}; //TODO why is this needed? 
+            for (const station of response.data.stationBeanList) {
+                if (this.isFavorite(station)) {
+                    const id = station.id.toString()
+                    station.name = this.favs[id];
+                    this.stations[id] = station;
+                }
+            }
+            this.genRoutes();
         }, response => {
-            this.stations = []
+            this.stations = {}
             console.log("Error: " + response)
         });
     }
